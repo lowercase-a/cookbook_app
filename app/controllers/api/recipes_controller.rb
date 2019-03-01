a745bdeda79ebe812a3fb8148d6abc398549b944
Brian <riceb53@gmail.com>
2019-02-28 21:13:31 -0600

    add show action

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -3,4 +3,10 @@ class Api::RecipesController < ApplicationController
     @recipes = Recipe.all
     render 'index.json.jbuilder'
   end
+
+  def show
+    the_id = params[:id]
+    @recipe = Recipe.find_by(id: the_id)
+    render 'show.json.jbuilder'
+  end
 end
