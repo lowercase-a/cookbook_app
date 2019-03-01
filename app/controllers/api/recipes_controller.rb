9928bdbd6c35af4c09746a9b0a55c4252618fb5a
Brian <riceb53@gmail.com>
2019-02-28 21:29:06 -0600

    add update action

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -11,8 +11,29 @@ class Api::RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.new(title: "eggs", chef: "Brian", ingredients: "eggs...", prep_time: 15, directions: "heat the eggs")
+    @recipe = Recipe.new(
+      title: params[:input_title],
+      chef: params[:input_chef],
+      ingredients: params[:input_ingredients],
+      prep_time: params[:input_prep_time],
+      directions: params[:input_directions]
+    )
     @recipe.save
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    # find the recipe
+    the_id = params[:id]
+    @recipe = Recipe.find_by(id: the_id)
+    # modify the recipe
+    @recipe.update(
+      title: params[:input_title],
+      chef: params[:input_chef],
+      ingredients: params[:input_ingredients],
+      prep_time: params[:input_prep_time],
+      directions: params[:input_directions]
+    )
     render 'show.json.jbuilder'
   end
 end
