54778fb43b1ea5468de807c39ba2db36a09602de
Brian <riceb53@gmail.com>
2019-03-21 20:19:41 -0500

    erb make index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+  get '/recipes' => 'recipes#index'
+
+
   namespace :api do
     get '/recipes' => 'recipes#index'
     get '/recipes/:id' => 'recipes#show'
